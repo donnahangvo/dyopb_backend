7d8c8b93e06f611457827663188413f1169e54be
Donna Vo <you@example.com>
2024-03-11 16:37:58 -0400

    changed allowed hosts

--- dyopb_backend/settings.py
+++ dyopb_backend/settings.py
@@ -29,17 +29,22 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-unsecure-not-secret-key-just-using-this-for-development-purposes"
+# SECRET_KEY = "django-unsecure-not-secret-key-just-using-this-for-development-purposes"
 
-# SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-# DEBUG = os.environ.get("DEBUG", "False").lower() == "True"
+# DEBUG = True
+
+DEBUG = os.environ.get("DEBUG", "False").lower() == "True"
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1:8000',
+    'dyopb-backend.onrender.com']
+
 # ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(" ")
 
 EMAIL_HOST = 'localhost'
