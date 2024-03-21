f0835143425fb9030850de6031df759954c78c01
Donna Vo <you@example.com>
2024-03-20 23:34:21 -0400

    modified category and product urls

--- apps/store/api/urls.py
+++ apps/store/api/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('variation/', variation_list, name='variation_list'),
     path('option/', option_list, name='option_list'),
     path('specification/', specification_list, name='specification_list'),
-    path('product/<str:category_slug>/<str:slug>/', product_detail, name='product_detail'),
-    path('category/<str:slug>/', category_detail, name='category_detail'),
+    path('product/', product_detail, name='product_detail'),
+    path('category/', category_detail, name='category_detail'),
     path('product/<str:category_slug>/<str:slug>/review/', product_review, name='product_review'),
 ]
